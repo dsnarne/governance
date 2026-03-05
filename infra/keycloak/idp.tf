f11563f55ce1adfc70d61fcd1466f5d3bb684eed
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-04 20:25:10 -0500

    feat(iac): manage SAML IDP

---
+++ infra/keycloak/idp.tf
@@ -0,0 +1,37 @@
+resource "keycloak_saml_identity_provider" "cmu_saml" {
+  realm        = keycloak_realm.labrador.realm
+  alias        = "cmu-saml"
+  display_name = "CMU SAML"
+
+  entity_id                  = "https://idp.scottylabs.org/realms/labrador"
+  single_sign_on_service_url = "https://login.cmu.edu/idp/profile/SAML2/POST/SSO"
+
+  name_id_policy_format = "Transient"
+  principal_type        = "FRIENDLY_ATTRIBUTE"
+  principal_attribute   = "eduPersonPrincipalName"
+
+  post_binding_authn_request = true
+  post_binding_response      = true
+  want_authn_requests_signed = true
+
+  signature_algorithm = "RSA_SHA256"
+
+  xml_sign_key_info_key_name_transformer = "KEY_ID"
+  validate_signature                     = true
+
+  authn_context_comparison_type = "exact"
+  store_token                   = false
+  trust_email                   = true
+  first_broker_login_flow_alias = "Auto-link LDAP users"
+  sync_mode                     = "FORCE"
+
+  # Can't find in the UI but there when imported
+  login_hint = "false"
+
+  extra_config = {
+    idpEntityId              = "https://login.cmu.edu/idp/shibboleth"
+    metadataDescriptorUrl    = "https://login.cmu.edu/idp/shibboleth"
+    useMetadataDescriptorUrl = "true"
+    allowCreate              = "true"
+  }
+}
