61b5b5388a49bc42dd14240a3ad192248e6e854e
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-11 17:11:55 -0400

    feat(iac): add more user profile fields

--- infra/keycloak/user_profile.tf
+++ infra/keycloak/user_profile.tf
@@ -10,7 +10,6 @@ resource "keycloak_realm_user_profile" "userprofile" {
     name         = keycloak_user_template_importer_identity_provider_mapper.username.name
     display_name = "$${username}"
 
-
     permissions {
       view = local.view_permissions
       edit = local.edit_permissions
@@ -48,6 +47,16 @@ resource "keycloak_realm_user_profile" "userprofile" {
   }
 
   attribute {
+    name         = keycloak_ldap_user_attribute_mapper.middle_name.user_model_attribute
+    display_name = "Middle Name"
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
     name         = keycloak_ldap_user_attribute_mapper.last_name.user_model_attribute
     display_name = "$${lastName}"
 
@@ -61,7 +70,6 @@ resource "keycloak_realm_user_profile" "userprofile" {
     name         = keycloak_ldap_user_attribute_mapper.full_name.user_model_attribute
     display_name = "Full Name"
 
-
     permissions {
       view = local.view_permissions
       edit = local.edit_permissions
@@ -72,6 +80,15 @@ resource "keycloak_realm_user_profile" "userprofile" {
     name         = keycloak_ldap_user_attribute_mapper.display_name.user_model_attribute
     display_name = "Display Name"
 
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.orcid.user_model_attribute
+    display_name = "ORCID"
 
     permissions {
       view = local.view_permissions
@@ -79,10 +96,20 @@ resource "keycloak_realm_user_profile" "userprofile" {
     }
   }
 
-  # --- CMU Extended Metadata ---
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.primary_affiliation.user_model_attribute
+    display_name = "Primary Affiliation"
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
   attribute {
     name         = keycloak_ldap_user_attribute_mapper.affiliations.user_model_attribute
     display_name = "Affiliations"
+    multi_valued = true
 
     permissions {
       view = local.view_permissions
@@ -93,6 +120,7 @@ resource "keycloak_realm_user_profile" "userprofile" {
   attribute {
     name         = keycloak_ldap_user_attribute_mapper.departments.user_model_attribute
     display_name = "Departments"
+    multi_valued = true
 
     permissions {
       view = local.view_permissions
@@ -103,6 +131,17 @@ resource "keycloak_realm_user_profile" "userprofile" {
   attribute {
     name         = keycloak_ldap_user_attribute_mapper.colleges.user_model_attribute
     display_name = "Colleges"
+    multi_valued = true
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.level.user_model_attribute
+    display_name = "Level"
 
     permissions {
       view = local.view_permissions
@@ -114,6 +153,36 @@ resource "keycloak_realm_user_profile" "userprofile" {
     name         = keycloak_ldap_user_attribute_mapper.class.user_model_attribute
     display_name = "Class"
 
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.status.user_model_attribute
+    display_name = "Status"
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.creation_date.user_model_attribute
+    display_name = "Creation Date"
+
+    permissions {
+      view = local.view_permissions
+      edit = local.edit_permissions
+    }
+  }
+
+  attribute {
+    name         = keycloak_ldap_user_attribute_mapper.modify_date.user_model_attribute
+    display_name = "Modify Date"
 
     permissions {
       view = local.view_permissions
