801d1f9705ba43f6e92f6b6c007901c8c4b5b370
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-04-01 22:32:43 -0400

    feat(iac): add github org setting

---
+++ infra/github/main.tf
@@ -0,0 +1,5 @@
+resource "github_organization_settings" "labrador" {
+  name          = "ScottyLabs | Labrador"
+  description   = "ScottyLabs Labrador Team Projects"
+  billing_email = "ops@scottylabs.org"
+}
